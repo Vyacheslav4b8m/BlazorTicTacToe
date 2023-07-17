f860a4e8ca47bdd7efa801b1096c7ac15b82caeb
ЦойВячеслав <@204-6>
2023-07-17 18:19:19 +0600

    добавлено логику победы

--- BlazorApp1/domain/Board.cs
+++ BlazorApp1/domain/Board.cs
@@ -4,9 +4,20 @@
     {
         Blank, X, O
     }
-
+    public enum GameResult
+    {
+        Xwon,
+        Owom,
+        Draw
+    }
+    public enum Gamer
+    {
+        X,
+        O
+    }
     public class Board
     {
+       
         public int ColumCount => 3;
         public int RowCount => 3;
         public CellState[,] Cells { get; set; }
@@ -14,28 +25,79 @@
         {
             Cells = new CellState[RowCount, ColumCount];
         }
+        public CellState CurrentTurn = CellState.X;
 
-        public CellState Celll_0_0 { get; set; } = CellState.X;
-        public CellState Cell_0_1 { get; set; }
-        public CellState Cell_0_2 { get; set; }
-        public CellState Cell_1_0 { get; set; }
-        public CellState Cell_1_1 { get; set; }
-        public CellState Cell_1_2 { get; set; }
-        public CellState Cell_2_0 { get; set; }
-        public CellState Cell_2_1 { get; set; }
-        public CellState Cell_2_2 { get; set; }
+        //тернарный опретор
+        public void NextTurns()
+        {
+            CurrentTurn = CurrentTurn == CellState.X ? CellState.O : CellState.X;
+        }
 
-    public void CellClick(int row, int column) 
-    {
-    if (Cells[row, column] == CellState.Blank)
-    {
-     Cells[row, column] = CellState.X;
-    }
-    else if (Cells[row, column] == CellState.Blank) {
-    Cells [row, column] = CellState.O;
-    }
+        public void CellClick(int row, int column)
+        {
+            if (Cells[row, column] == CellState.Blank)
+            {
+                Cells[row, column] = CurrentTurn;
+                NextTurns();
+            }
+        }
 
-    }
+        public GameResult GetGameResult()
+        {
+            if(GameCheck(Gamer.X))
+            {
+                return GameResult.Xwon;
+            }
+            else if(GameCheck(Gamer.O))
+            {
+                return GameResult.Owom;
+            }
+            return GameResult.Draw;
 
+        }
+        public bool GameCheck(Gamer gamer)
+        {
+            CellState expectedCellState;
+            if (gamer == Gamer.X)
+                expectedCellState = CellState.X;
+            else
+                expectedCellState = CellState.O;
+            for(int column = 0; column < ColumCount; column++)
+            {
+                var expcount =0;
+                for (int row = 0; row < RowCount; row++)
+                {
+                    if(Cells[row, column] == expectedCellState)
+                    {
+                        expcount++;
+                    }
+                    if(expcount == 3)
+                    {
+                        return true;
+                    }
+                }
+            }
+            //if (Cells[0, 0] == expectedCellState && Cells[1, 0] == expectedCellState && Cells[2, 0] == expectedCellState)
+            //{
+            //    return true;
+            //}
+            //if (Cells[0, 1] == expectedCellState && Cells[1, 1] == expectedCellState && Cells[2, 1] == expectedCellState)
+            //{
+            //    return true;
+            //}
+            //if (Cells[0, 2] == expectedCellState && Cells[1, 2] == expectedCellState && Cells[2, 2] == expectedCellState)
+            //{
+            //    return true;
+            //}
+            //if (Cells[0, 0] == expectedCellState && Cells[1, 1] == expectedCellState && Cells[2, 2] == expectedCellState)
+            //{
+            //    return true;
+            //}
+            //if (Cells[2, 0] == expectedCellState && Cells[1, 1] == expectedCellState && Cells[0, 2] == expectedCellState)
+            //{
+            //    return true;
+            //}
+            return false;
+        }
     }
 }
